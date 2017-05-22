a089a41174e130b1b1c17a3b61580fd6c18d4ab2
caosonggen <aintchannel@163.com>
2017-05-22 18:07:50 +0800

    初始化项目

---
+++ src/components/firstcomponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="firstcomponent">
+    <h1> i am first</h1>
+    <a>written by {{ author }}</a>
+  </div>
+</template>
+<script>
+  export default {
+      data() {
+          return {
+              author: 'crewkicks'
+          }
+      }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,50 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+import VueRouter from 'vue-router'
+import VueResourse from 'vue-resource'
+import ElementUI from 'element-ui'; //引入饿了么框架
+import 'element-ui/lib/theme-default/index.css'
+//开启DEBUG模式
+Vue.config.debug = true;
+Vue.use(VueRouter);
+Vue.use(VueResourse);
+Vue.use(ElementUI);
+Vue.config.productionTip = true;
+
+//定义组件
+// const First = { template: '<div><h2>我是第 1 个子页面</h2></div>' };
+// import secondecomponent from 'components/secondcomponent.vue'
+//
+// //创建一个路由器实例
+// //配置路由规则
+// const router = new VueRouter ({
+//   mode: 'history',
+//   base: __dirname,
+//   routes: [
+//     {
+//       path: '/first',
+//       component: First
+//     },
+//     {
+//       path: '/second',
+//       component: secondecomponent
+//     }
+//   ]
+// });
+//
+// //创建应用，创建路由器实例，挂载到#app的实例上
+// const app =new Vue ({
+//   router: router,
+//   render: h=>h(App)
+// }).$mount('#app');
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
